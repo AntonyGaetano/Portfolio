de4ba765c8f711fa7e5cf5a05accbaf17c65cd37
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-09-06 20:37:45 -0300

    Atualização
    Foi feito alguns teste com o javascript, de como ficará a parte onde ficarão os projetos do portfolio

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const MouseOver=()=>{
+   var tag = document.getElementById('p')
+   tag.style.visibility="visible"
+}
+
+const MouseOut=()=>{
+    var tag = document.getElementById('p')
+    tag.style.visibility="hidden"
+}
+
+document.getElementById("projeto1").addEventListener('mouseover',MouseOver);
+
+document.getElementById("projeto1").addEventListener('mouseout',MouseOut);
