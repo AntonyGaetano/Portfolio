33172b53ee2d7c2a5449ea47857095d6915dfbcb
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-09-12 08:20:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-/*
-const MouseOver=()=>{
-   var tag = document.getElementById('p')
-   tag.style.visibility="visible"
+
+var box = document.getElementById('box1');
+var icone_menu = document.getElementById("i_menu");
+var icone_arrow = document.getElementById('icone_seta');
+var header = document.getElementById("header");
+
+//Chama de funções
+document.getElementById('btn-ver-projeto').addEventListener("click",Box1)
+
+
+document.getElementById('box-header').addEventListener('click', ReverseBox1); 
+
+//Função para fazer a transição do Box, após o clique no botão
+function Box1(){
+    box.style.height="60px";   
+    tmp = setTimeout(ADD_i,1500)
 }
 
-const MouseOut=()=>{
-    var tag = document.getElementById('p')
-    tag.style.visibility="hidden"
+//Função para fazer o icone da seta ficar visível
+function ADD_i(){
+header.style.justifyContent="center";
+icone_menu.style.display="none";
+icone_arrow.style.display="inline-block";
 }
 
-document.getElementById("projeto1").addEventListener('mouseover',MouseOver);
-
-document.getElementById("projeto1").addEventListener('mouseout',MouseOut); */
+//Função para fazer a transição do Box, após o clique no botão
+function ReverseBox1(event){
+  if(event.target.id == 'icone_seta'){
+    box.style.height="657px";
+    icone_arrow.style.display='none';
+    icone_menu.style.display="inline-block";
+    header.style.justifyContent="right";
+  }
+}
