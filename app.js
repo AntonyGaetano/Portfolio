4cffbe911b3a18d84136ad31432078bb1dad3f7b
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-10-08 14:44:01 -0300

    ...

--- app.js
+++ app.js
@@ -1,62 +1,56 @@
 
-var box1 = document.getElementById('box1');
-var box2 = document.getElementById('box2');
-var main = document.getElementById('main1')
-var icone_menu = document.getElementById("i_menu");
-var icone_arrow = document.getElementById('icone_seta');
-var icone_menu = document.getElementById('i_menu');
-var boxheader = document.getElementById('box-header')
-var ul = document.getElementById('ul');
-var header = document.getElementById("header");
-var icone_sobe_header = document.getElementById('seta-box-header');
+var box1 = document.getElementById('box1'); //Parte inicial
+
+document.getElementById('btn-ver-projeto').addEventListener("click",Box1); //Botão ver projetos
+
+var icone_sobe_header = document.getElementById('seta-box-header'); // Icone que aparece quando clica no menu
+icone_sobe_header.addEventListener("click",Menu);  // Chamada da função Menu, após o clique
+
+var boxheader = document.getElementById('box-header'); // Box responsavel pelo o header
+
+var box2 = document.getElementById('box2'); //Box onde fica os projetos e as demais sections
+
+var icone_menu = document.getElementById("i_menu"); // Icone menu que aparece assim na tela inicial
+icone_menu.addEventListener('click',Menu); // Chamada da função Menu, após o clique
+
+var icone_arrow = document.getElementById('icone_seta'); // Icone que aprece assim que clica no botao ver projetos
+icone_arrow.addEventListener('click', ReverseBox1);  // Chamada da funçao para voltar tela inicial
 
 
+var ul = document.getElementById('ul'); // Responsavel pela lista não ordenada que fica no menu
 
-var BLOCOS = document.querySelectorAll('div.blocos-div');
-for(let k = 0; k < BLOCOS.length; k++){
-  BLOCOS[k].addEventListener('click',MostraProjeto)
-}
-
-icone_menu.addEventListener('click',Menu);
-icone_sobe_header.addEventListener('click',Menu);
-
-//Chama de funções
-document.getElementById('btn-ver-projeto').addEventListener("click",Box1);
-document.getElementById("i_fechar").addEventListener("click",FecharProjeto);
-
-boxheader.addEventListener('click', ReverseBox1); 
-
-document.getElementById("li_sobre").addEventListener("click",Sobre);
-document.getElementById("li_habilidades").addEventListener("click",Sobre);
-document.getElementById("li_contato").addEventListener("click",Sobre);
+var header = document.getElementById("header"); // Header por si só
 
 
-//Função para fazer a transição do Box, após o clique no botão
+document.getElementById("li_sobre").addEventListener("click",Sobre); //Aqui é o li sobre do menu
+
+document.getElementById("li_habilidades").addEventListener("click",Sobre); //Aqui é o li habilidades do menu
+
+document.getElementById("li_contato").addEventListener("click",Sobre); //Aqui é o li contato do menu
+
+
+//Função para fazer a transição do Box, após o clique no botão ver projetos
 function Box1(){
-
   document.body.style.padding="none"
   document.body.style.margin="none"
   document.body.style.width=""
   document.body.style.height=""
 
-    box1.style.height="50px";  
-    ul.style.display="none"; 
-    icone_sobe_header.style.display="none"
-    tmp = setTimeout(ADD_i,900);
-
+  box1.style.height="50px";  
+  ul.style.display="none"; 
+  icone_sobe_header.style.display="none"
+  tmp = setTimeout(
+    ()=>{
+       boxheader.style.justifyContent="center";
+       icone_menu.style.display="none";
+       icone_arrow.style.display="inline-block";
+       box2.style.display="flex"; 
+    },900);
 }
 
-//Função para fazer o icone da seta ficar visível
-function ADD_i(){
-boxheader.style.justifyContent="center";
-icone_menu.style.display="none";
-icone_arrow.style.display="inline-block";
-box2.style.display="flex";
-}
 
-//Função para fazer a transição do Box, após o clique no botão
+//Função para fazer a transição do Box, após o clique na seta, para voltar tela inicial 
 function ReverseBox1(event){
-  if(event.target.id == 'icone_seta'){
     box1.style.height="657px";
     icone_arrow.style.display='none';
     icone_menu.style.display="inline-block";
@@ -66,11 +60,96 @@ function ReverseBox1(event){
     document.body.style.padding="0"
     document.body.style.margin="0"
     document.body.style.width="100%"
-    document.body.style.height="100%"
+    document.body.style.height="100%" 
+}
+
+//Função para mostar o menu
+var cont = 0
+function Menu(){
+  if(cont == 0){
+    var altura = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+     ul.style.display="flex";
+     header.style.height=altura+"px";
+     header.style.backgroundColor="cornflowerblue";
+     header.style.border="1px solid";
+     icone_menu.innerHTML="";
+     icone_sobe_header.style.display="flex";
+
+  cont = 1;
+  }
+  else{
+     ul.style.display="none";
+     header.style.height="3rem";
+     icone_menu.innerHTML="menu";
+     header.style.backgroundColor="";
+     header.style.border="";
+     icone_sobe_header.style.display="none";
+
+  cont = 0;
   }
 }
 
+
+//Funçao para fazer a rolagem da tela até o id informado!
+
+function Sobre(event){
+    let id  = event.target.id;
+
+   if(id == "li_sobre"){
+
+      Menu();
+      tempo = setTimeout(Box1,800)
+      tmp = setTimeout(()=>{
+       // selecionar elemento
+         let el = document.getElementById('sobre');
+       // utiliza método
+         let elCoordenadas = el.getBoundingClientRect(); 
+         window.scrollTo(0,elCoordenadas.y);
+      },2200)
+
+   }else if(id == "li_habilidades"){
+    
+       Menu();
+       tempo = setTimeout(Box1,800)
+       tmp = setTimeout(()=>{
+       // selecionar elemento
+       let el = document.getElementById('habilidades');
+       // utiliza método
+       let elCoordenadas = el.getBoundingClientRect();
+
+       window.scrollTo(0,elCoordenadas.y);
+      },2200)
+
+  }else{
+       
+     Menu();
+     tempo = setTimeout(Box1,800)
+     tmp = setTimeout(()=>{
+     // selecionar elemento
+     let el = document.getElementById('contato');
+     // utiliza método
+     let elCoordenadas = el.getBoundingClientRect();
+
+     window.scrollTo(0,elCoordenadas.y);
+  },2200)
+  }
+}
+
+
+function Inicial(){
+  document.body.style.padding="0"
+  document.body.style.margin="0"
+  document.body.style.width="100%"
+  document.body.style.height="100%"
+}
+
+ 
+window.addEventListener("load",Inicial);
+
+
+
 //Criação do objeto com as informações do projeto1
+/*
 var bloco1 = {
   num_projeto:"Projeto 1",
   nome_projeto:"Calculadora De IMC",
@@ -114,167 +193,4 @@ var link_projetos =
 "https://conversor-de-temperatura.netlify.app",
 "https://jogo-pedra-papel-e-tesoura.netlify.app/",
 "https://cronometroviajavascript.000webhostapp.com/html/indxe.html ",
-"https://lista-de-tarefas-personalizada.netlify.app"];
-
-
-var cont = 0
-function Menu(){
-  if(cont == 0){
-
-    var altura = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
-  
-
-  ul.style.display="flex";
-  header.style.height=altura+"px";
-  header.style.backgroundColor="cornflowerblue";
-  header.style.border="1px solid";
-  icone_menu.innerHTML="";
-  icone_sobe_header.style.display="flex"
-  main.style.opacity=0.3;
-  cont = 1;
-  }
-  else{
-    ul.style.display="none";
-    header.style.height="3rem";
-    icone_menu.innerHTML="menu";
-    header.style.backgroundColor="";
-    header.style.border="";
-    icone_sobe_header.style.display=""
-    main.style.opacity=1;
-    cont = 0;
-  }
-}
-
- 
-var ChamaProjeto = 0;
-
-function MostraProjeto(event){
-  
-  if(ChamaProjeto == 0){
-
-  let id = event.target.id;
-  var num_obj = id.replace(/\D/g,'');
-  let Obj_bloco = [bloco1,bloco2,bloco3,bloco4,bloco5, bloco6];
-  
-
-  document.getElementById("box-suspenso").style.display="flex";
-  var box = document.getElementById("box_interior");
-  var h2 = document.getElementById("nome-projeto");
-
-  var div_img_desc = document.createElement("div");
-  var div_img = document.createElement("div");
-  var div_tec_det = document.createElement('div');
-  var div_tec = document.createElement('div');
-  var div_det = document.createElement('div');
-  var div_desc = document.createElement('div');
-
-  var h5 = document.createElement('h5');
-  var h4 = document.createElement('h4');
-  var detalhe = document.createElement('h5'); 
-  var btn = document.createElement('button');
-  var a = document.createElement("a");
-
-  a.appendChild(btn)
-  div_desc.appendChild(h5)
-  div_img_desc.appendChild(div_img);
-  div_img_desc.appendChild(div_desc);
-
-  div_tec.appendChild(h4)
-
-  div_det.appendChild(detalhe);
-  div_det.appendChild(a);
-
-  div_tec_det.appendChild(div_tec)
-  div_tec_det.appendChild(div_det)
-
-  box.appendChild(div_img_desc);
-  box.appendChild(div_tec_det);
- 
-  div_img.classList.add("img-info");
-  div_img_desc.classList.add("img-descricao");
-  div_desc.classList.add('descricao')
-  div_tec.classList.add("tecnologia");
-  div_det.classList.add('detalhe')
-  div_tec_det.classList.add('tecnologia-detalhe')
-  
- 
-  document.getElementById("i_fechar").addEventListener("click",()=>{
-    div_img_desc.remove()
-    div_img.remove()
-    div_tec_det.remove()
-    ChamaProjeto = 0;
-  });
- 
-
-  h2.innerHTML = Obj_bloco[num_obj-1].nome_projeto;
-  div_img.style.backgroundImage = "url(fotos_projetos/projeto"+num_obj+".png)";
-  h5.innerHTML = Obj_bloco[num_obj-1].descricao;
-  detalhe.innerHTML = "Para mais detalhes, clique abaixo!"
-  btn.innerHTML = "Ver projeto"; 
-  a.href = link_projetos[num_obj-1]
-
-  ChamaProjeto = 1;
- }
-
-}
-
-function FecharProjeto(){
-  document.getElementById("box-suspenso").style.display="none";  
-}
-
-
-function Inicial(){
-  document.body.style.padding="0"
-  document.body.style.margin="0"
-  document.body.style.width="100%"
-  document.body.style.height="100%"
-}
-
-
-function Sobre(event){
-   let id  = event.target.id;
-
-   if(id == "li_sobre"){
-
-     Menu();
-     tempo = setTimeout(Box1,800)
-     tmp = setTimeout(()=>{
-     // selecionar elemento
-     let el = document.getElementById('sobre');
-     // utiliza método
-     let elCoordenadas = el.getBoundingClientRect(); 
-     window.scrollTo(0,elCoordenadas.y);
-    },2200)
-  }
-  else if(id == "li_habilidades"){
-    
-   Menu();
-     tempo = setTimeout(Box1,800)
-     tmp = setTimeout(()=>{
-    // selecionar elemento
-     let el = document.getElementById('habilidades');
-     // utiliza método
-     let elCoordenadas = el.getBoundingClientRect();
-
-    window.scrollTo(0,elCoordenadas.y);
-    },2200)
-  }
-
-  else{
-       
-   Menu();
-   tempo = setTimeout(Box1,800)
-   tmp = setTimeout(()=>{
-  // selecionar elemento
-   let el = document.getElementById('contato');
-   // utiliza método
-   let elCoordenadas = el.getBoundingClientRect();
-
-  window.scrollTo(0,elCoordenadas.y);
-  },2200)
-  }
-}
-
-window.addEventListener("load",Inicial);
+"https://lista-de-tarefas-personalizada.netlify.app"];   */
